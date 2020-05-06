76f86a6e01cc25ee726b9937ed203ab1fc3232f0
Dallas Blackwell <mdvayne923@gmail.com>
2020-05-06 18:14:55 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -7,26 +7,13 @@ def unsafe?(speed)
     return speed = "unsafe"
   else speed
     return "safe"
-    binding.pry
+  end
 end
 
 def not_safe?(speed)
 	if speed < 40 ? not_safe : safe
+	  return = ""
   elsif speed > 60 ? unsafe : safe
-  # binding.pry
-end
-	
-def speak_to_grandma(phrase)
-  if phrase == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!"
-<<<<<<< HEAD
-  elsif phrase != phrase.upcase
-=======
-    elsif phrase == "Hi!"
->>>>>>> f2af94d1454f2291df3dc4f050dbb8077758f97a
-    return "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == phrase.upcase
-    return "NO, NOT SINCE 1938!"
   end
 end
-
+	
