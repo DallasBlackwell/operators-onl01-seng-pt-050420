99dedc82b2dba60fd00c98df8dea403aa23eabab
Dallas Blackwell <mdvayne923@gmail.com>
2020-05-06 18:07:01 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,19 @@
-def unsafe?(speed)
+require 'pry'
 
+def unsafe?(speed)
+  if speed < 40
+    puts speed = "unsafe"
+  elsif speed > 60
+    puts speed = "unsafe"
+  else speed
+    puts "safe"
+    binding.pry
 end
 
-
-
 def not_safe?(speed)
-	
+	if speed < 40 ? not_safe : safe
+  elsif speed > 60 ? unsafe : safe
+  # binding.pry
 end
 	
 
