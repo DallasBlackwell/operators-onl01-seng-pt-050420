e1fa1a959f132afba4e30e521fdebc57098196d2
Dallas Blackwell <mdvayne923@gmail.com>
2020-05-06 18:28:49 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -8,11 +8,11 @@ def unsafe?(speed)
   else speed
     return "safe"
   end
+  binding.pry
 end
 
 def not_safe?(speed)
 	if speed < 40 ? not_safe : safe
-	  return = ""
   elsif speed > 60 ? unsafe : safe
   end
 end
